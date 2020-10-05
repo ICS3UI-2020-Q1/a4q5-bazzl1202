6fe81568621bd9b17afec1fb9851289bbe7e0a1a
bazzl1202 <bazzl1202@wrdsb.ca>
2020-10-05 13:22:17 +0000

    Now prints out the hailstone sequence

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Asks user for intger and prints out Hailstone sequence
+ * @author Liana Bazzarella
  */
 public class Main {
 
@@ -9,7 +10,27 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // create scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask the user for an integer
+    System.out.println("Please enter a positive integer to see the hailstone sequence.");
+    int integer = input.nextInt();
+
+    // tells the user the hailstone sequence is starting
+    System.out.println("Hailstone sequence is the following:");
+    System.out.println(integer);
+
+    // hailstone sequence operations
+    while(integer > 1){
+      if( integer%2 == 0){
+      integer = integer/2;
+      System.out.println(integer);
+    } else if( integer%2 == 1){
+      integer = 3*integer + 1;
+      System.out.println(integer);
+    }
+    }
+  
   }
 }
